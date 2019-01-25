0535235a8dfe11bcdf0c3daab25d4a6429eafa0b
qingpan.zhang.o <Ss1234567890>
2019-01-25 14:25:04 +0800

    init

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,11 @@
 import {Component, Provide, Vue} from 'vue-property-decorator';
 import {CreateElement} from 'vue';
-// import * as homeStyle from './home.module.scss';
+import * as homeStyle from './home.module.scss';
 @Component({})
 export default class Home extends Vue {
   private render(h: CreateElement) {
     return (
-      <div >
+      <div class={homeStyle.content}>
         <img src={require('../assets/logo.png')}/>
         <div msg={'Welcome to Your Vue.js + TypeScript App'}/>
       </div>
